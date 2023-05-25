625eca3607aa61a98505b804837f4245ac67932a
takafumi.noiri <takafumi.noiri@rakuten.com>
2023-05-25 22:25:28 +0900

    Initial Commit

---
+++ English-Learning-iOS/English_Learning_iOSApp.swift
@@ -0,0 +1,18 @@
+//
+//  English_Learning_iOSApp.swift
+//  English-Learning-iOS
+//
+//  Created by takafumi11 on 2023/05/25.
+//  
+//
+
+import SwiftUI
+
+@main
+struct English_Learning_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ English-Learning-iOSUITests/English_Learning_iOSUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  English_Learning_iOSUITestsLaunchTests.swift
+//  English-Learning-iOSUITests
+//
+//  Created by takafumi11 on 2023/05/25.
+//  
+//
+
+import XCTest
+
+final class English_Learning_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
